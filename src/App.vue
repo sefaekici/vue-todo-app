d3e9ccb71e05c92beb5d79a2f982c80dc388df70
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-05-17 16:52:54 +0300

    Style İşlemleri

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,33 @@
   <div id="app">
     
 
+    <div class="todo-app">
+
+      <div class="add-todo">
+        <input class="todoAddInput" type="text">
+        <button class="todoAddButton">+</button>
+      </div>
+
+
+
+      <div class="todos">
+
+        <h2>Yapılacaklar Listesi</h2>
+        <ul>
+          <li>merhaba</li>
+          <li>merhaba</li>
+          <li>merhaba</li>
+          <li>merhaba</li>
+          <li>merhaba</li>
+          <li>merhaba</li>
+          <li>merhaba</li>
+          <li>merhaba</li>
+          <li>merhaba</li>
+        </ul>
+      </div>
+
+    </div>
+
 
   </div>
 </template>
@@ -13,6 +40,92 @@ export default {
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+}
+html,body{
+  width: 100%;
+  height: 100%;
+}
+
+body{
+  background: #ffe2e2;
+}
+
+#app{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.todo-app{
+  width: 500px;
+  min-height: 600px;
+  border-radius: 1rem;
+  background: #f6f6f6;
+}
+
+
+.add-todo{
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 30px;
+}
+
+.todoAddInput{
+  width: 80%;
+  height: 50px;
+  border-radius: 1rem;
+  padding: 0 20px;
+  font-size: 24px;
+  outline: none;
+
+}
+.todoAddButton{
+  width: 15%;
+  font-size: 30px;
+  background: #8785a2;
+  border-radius: 10px;
+  color: #fff;
+}
+
+.todos{
+  display: flex;
+  flex-direction: column;
+  margin-top: 1.5rem;
+}
+
+.todos h2{
+  text-align: center;
+  margin-bottom: 1rem;
+  color:#8785a2;
+}
+
+.todos ul{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 1rem 3rem;
+  margin-bottom: 1.5rem !important;
+  
+}
+
+.todos ul li{
+  margin: 10px;
+  padding: 0.5rem;
+  border: 2px solid #8785a2;
+  border-radius: 10px;
+}
+
+
+
+
 
 
 </style>
